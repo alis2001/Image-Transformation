134774a2778a4a19d5cbad8222547a245262efd8
ctu <ctu@softwarecare.it>
2025-10-06 11:27:14 +0200

    completed the task

--- OVhnUjkHEemPcBIa2xz0qA_3962c780390711e9a0ba65a630086e0a_image-transform-given-files_20190225/ImageTransform.cpp
+++ OVhnUjkHEemPcBIa2xz0qA_3962c780390711e9a0ba65a630086e0a_image-transform-given-files_20190225/ImageTransform.cpp
@@ -1,107 +1,91 @@
-#include <iostream>
-#include <cmath>
-#include <cstdlib>
-
+#include "ImageTransform.h"
 #include "uiuc/PNG.h"
 #include "uiuc/HSLAPixel.h"
-#include "ImageTransform.h"
+#include <cmath>
 
-/* ******************
-(Begin multi-line comment...)
+using namespace uiuc;
 
-Write your name and email address in the comment space here:
-
-Name:
-Email:
-
-(...end multi-line comment.)
-******************** */
-
-using uiuc::PNG;
-using uiuc::HSLAPixel;
-
-/**
- * Returns an image that has been transformed to grayscale.
- *
- * The saturation of every pixel is set to 0, removing any color.
- *
- * @return The grayscale image.
- */
 PNG grayscale(PNG image) {
-  /// This function is already written for you so you can see how to
-  /// interact with our PNG class.
   for (unsigned x = 0; x < image.width(); x++) {
     for (unsigned y = 0; y < image.height(); y++) {
       HSLAPixel & pixel = image.getPixel(x, y);
-
-      // `pixel` is a reference to the memory stored inside of the PNG `image`,
-      // which means you're changing the image directly. No need to `set`
-      // the pixel since you're directly changing the memory of the image.
       pixel.s = 0;
     }
   }
-
   return image;
 }
 
-
-
-/**
- * Returns an image with a spotlight centered at (`centerX`, `centerY`).
- *
- * A spotlight adjusts the luminance of a pixel based on the distance the pixel
- * is away from the center by decreasing the luminance by 0.5% per 1 pixel euclidean
- * distance away from the center.
- *
- * For example, a pixel 3 pixels above and 4 pixels to the right of the center
- * is a total of `sqrt((3 * 3) + (4 * 4)) = sqrt(25) = 5` pixels away and
- * its luminance is decreased by 2.5% (0.975x its original value).  At a
- * distance over 160 pixels away, the luminance will always decreased by 80%.
- * 
- * The modified PNG is then returned.
- *
- * @param image A PNG object which holds the image data to be modified.
- * @param centerX The center x coordinate of the crosshair which is to be drawn.
- * @param centerY The center y coordinate of the crosshair which is to be drawn.
- *
- * @return The image with a spotlight.
- */
 PNG createSpotlight(PNG image, int centerX, int centerY) {
-
+  for (unsigned x = 0; x < image.width(); x++) {
+    for (unsigned y = 0; y < image.height(); y++) {
+      HSLAPixel & pixel = image.getPixel(x, y);
+      
+      double dx = x - centerX;
+      double dy = y - centerY;
+      double distance = sqrt(dx * dx + dy * dy);
+      
+      double decrease = distance * 0.005;
+      if (decrease > 0.8) {
+        decrease = 0.8;
+      }
+      
+      pixel.l = pixel.l * (1 - decrease);
+    }
+  }
   return image;
-  
 }
- 
 
-/**
- * Returns a image transformed to Illini colors.
- *
- * The hue of every pixel is set to the a hue value of either orange or
- * blue, based on if the pixel's hue value is closer to orange than blue.
- *
- * @param image A PNG object which holds the image data to be modified.
- *
- * @return The illinify'd image.
-**/
 PNG illinify(PNG image) {
-
+  for (unsigned x = 0; x < image.width(); x++) {
+    for (unsigned y = 0; y < image.height(); y++) {
+      HSLAPixel & pixel = image.getPixel(x, y);
+      
+      double illiniOrange = 11;
+      double illiniBlue = 216;
+      
+      double distToOrange = fabs(pixel.h - illiniOrange);
+      double distToBlue = fabs(pixel.h - illiniBlue);
+      
+      if (distToOrange > 180) {
+        distToOrange = 360 - distToOrange;
+      }
+      if (distToBlue > 180) {
+        distToBlue = 360 - distToBlue;
+      }
+      
+      if (distToOrange < distToBlue) {
+        pixel.h = illiniOrange;
+      } else {
+        pixel.h = illiniBlue;
+      }
+    }
+  }
   return image;
 }
- 
 
-/**
-* Returns an immge that has been watermarked by another image.
-*
-* The luminance of every pixel of the second image is checked, if that
-* pixel's luminance is 1 (100%), then the pixel at the same location on
-* the first image has its luminance increased by 0.2.
-*
-* @param firstImage  The first of the two PNGs, which is the base image.
-* @param secondImage The second of the two PNGs, which acts as the stencil.
-*
-* @return The watermarked image.
-*/
 PNG watermark(PNG firstImage, PNG secondImage) {
-
+  unsigned width = firstImage.width();
+  unsigned height = firstImage.height();
+  
+  if (secondImage.width() < width) {
+    width = secondImage.width();
+  }
+  if (secondImage.height() < height) {
+    height = secondImage.height();
+  }
+  
+  for (unsigned x = 0; x < width; x++) {
+    for (unsigned y = 0; y < height; y++) {
+      HSLAPixel & basePixel = firstImage.getPixel(x, y);
+      HSLAPixel & stencilPixel = secondImage.getPixel(x, y);
+      
+      if (stencilPixel.l == 1.0) {
+        basePixel.l = basePixel.l + 0.2;
+        if (basePixel.l > 1.0) {
+          basePixel.l = 1.0;
+        }
+      }
+    }
+  }
   return firstImage;
 }
